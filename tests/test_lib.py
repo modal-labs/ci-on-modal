76e94a11222836f4fa277b5992c660119d3d94a4
Charles Frye <charles@modal.com>
2024-06-06 04:31:48 +0000

    remove dead code from previous, inferior pattern

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,24 +1,6 @@
-from pathlib import Path
-
-import modal
 import pytest
 
 import lib
-
-image = (
-    modal.Image.debian_slim()
-    .pip_install_from_requirements(Path(__file__).parent / "requirements.txt")
-    .pip_install_from_requirements(Path(__file__).parent / "requirements-dev.txt")
-    .copy_local_file(Path(__file__).parent / "pytest.ini")
-)
-app = modal.App("ci-testing", image=image)
-
-
-@app.function(gpu="any")
-def run_pytest():
-    import subprocess
-
-    subprocess.run(["pytest", "-vs", "-m", "modal"], check=True)
 
 
 @pytest.mark.modal
@@ -28,8 +10,3 @@ def test_torch_cuda():
 
 def test_torch_no_cuda():
     assert not lib.has_gpu()
-
-
-@app.local_entrypoint()
-def main():
-    run_pytest.remote()
